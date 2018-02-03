89aee440b0204de35b766813227fdd1a101545df
Zhenya Koval <koval.zhn@gmail.com>
2018-02-03 12:25:31 +0200

    Create project

---
+++ HomeTask/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HomeTask
+//
+//  Created by Zhenya Koval on 2/3/18.
+//  Copyright © 2018 Zhenya Koval. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
